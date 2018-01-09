6c5455db228f57f7faf2286e800dbcb4e1c79285
kraeml <kraeml@sesamestreet.franken.de>
2018-01-09 06:48:42 +0100

    Packages with Mainifest.ini

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='jupyter_c_kernel',
-      version='1.2.3',
+      version='1.2.4',
       description='Minimalistic C kernel for Jupyter',
       author='Brendan Rius',
       author_email='ping@brendan-rius.com',
       url='https://github.com/brendanrius/jupyter-c-kernel/',
       download_url='https://github.com/brendanrius/jupyter-c-kernel/tarball/1.2.1',
       packages=['jupyter_c_kernel'],
-      package_data={'jupyter_c_kernel': ['resources/']},
+      include_package_data=True,
       scripts=['jupyter_c_kernel/install_c_kernel'],
       keywords=['jupyter', 'notebook', 'kernel', 'c']
       )
